8ffcc5905302a23be802fa6386bd9bf62e541048
Nischal lamichhane <108085352+hehenischal@users.noreply.github.com>
2023-10-26 11:09:41 +0545

    refactor:color changes

--- carousel.js
+++ carousel.js
@@ -15,3 +15,23 @@ incrementButton.addEventListener('click', () => {
     const currentQuantity = parseInt(quantityInput.value, 10);
     quantityInput.value = currentQuantity + 1;
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const stars = document.querySelectorAll(".star");
+
+    stars.forEach((star, index) => {
+        star.addEventListener("click", () => {
+            // Set the rating (1 to 5 stars)
+            const rating = index + 1;
+
+            // Highlight selected stars
+            stars.forEach((s, i) => {
+                if (i <= index) {
+                    s.classList.add("selected");
+                } else {
+                    s.classList.remove("selected");
+                }
+            });
+        });
+    });
+});
+
